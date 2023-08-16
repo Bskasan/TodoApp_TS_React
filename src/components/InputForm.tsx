87becedadd64590385725fb00a032371e003bcec
Bskasan <b.kasan@yandex.com>
2023-08-16 21:35:35 +0300

    adding todo done

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -7,6 +7,10 @@ interface IInputForm {
 const InputForm: React.FC<IInputForm> = ({ addTodo }) => {
   const [task, setTask] = useState("");
 
+  const handleClick = () => {
+    addTodo(task);
+    setTask("");
+  };
   return (
     <div className="input-form">
       <input
@@ -14,8 +18,15 @@ const InputForm: React.FC<IInputForm> = ({ addTodo }) => {
         placeholder="Enter the todo..."
         type="text"
         maxLength={40}
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
       />
-      <button className="btn-hover btn-color" type="submit">
+      <button
+        className="btn-hover btn-color"
+        type="submit"
+        onClick={handleClick}
+        disabled={!task}
+      >
         Add New Todo
       </button>
     </div>
