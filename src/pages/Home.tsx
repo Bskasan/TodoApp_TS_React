c37d082e86bd8cf704049dd50e5ec95a37d7a93d
Bskasan <b.kasan@yandex.com>
2023-08-16 20:53:42 +0300

    post todo function done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,18 +5,30 @@ import TodoList from "../components/TodoList";
 
 const url = "https://64dcff14e64a8525a0f77a87.mockapi.io/todos";
 
-
+type AddFunc = (text: string) => void;
 
 const Home = () => {
   const [todos, setTodos] = useState<TodoType[]>([]);
 
   const getTodos = async () => {
     try {
-      const { data } = await axios.get<TodoType[]>(url); 
+      const { data } = await axios.get<TodoType[]>(url);
       setTodos(data);
     } catch (error) {
       console.log(error);
     }
+  };
+
+  const addTodo: AddFunc = async (text) => {
+    const newTodo = {
+      task: text,
+      isDone: false,
+    };
+
+    try {
+      await axios.post(url, newTodo);
+      getTodos();
+    } catch (error) {}
   };
 
   useEffect(() => {
