23cbe71d5205fd8cf460f61cb46b6ff2e1c2d9bf
Bskasan <b.kasan@yandex.com>
2023-08-16 20:17:42 +0300

    todo list interface and global ts todotype added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,21 +3,16 @@ import React, { useEffect, useState } from "react";
 import InputForm from "../components/InputForm";
 import TodoList from "../components/TodoList";
 
-const url = "https://64dcff14e64a8525a0f77a87.mockapi.io/:endpoint";
+const url = "https://64dcff14e64a8525a0f77a87.mockapi.io/todos";
 
-interface TodoType {
-  id: string | number;
-  task: string;
-  isDone: boolean;
-}
+
 
 const Home = () => {
   const [todos, setTodos] = useState<TodoType[]>([]);
 
   const getTodos = async () => {
     try {
-      const { data } = await axios.get(url);
-      console.log(data);
+      const { data } = await axios.get<TodoType[]>(url); 
       setTodos(data);
     } catch (error) {
       console.log(error);
@@ -26,13 +21,12 @@ const Home = () => {
 
   useEffect(() => {
     getTodos();
-  }),
-    [];
+  }, []);
 
   return (
     <div className="main">
       <InputForm />
-      <TodoList />
+      <TodoList todos={todos} />
     </div>
   );
 };
